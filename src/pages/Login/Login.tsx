1dd76b344fce6c1c7c8217c8f87a0c4d520b1815
Overnap <overnapworks@gmail.com>
2022-06-04 14:50:52 +0900

    Add login layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,37 @@
-import React from "react"
+import { sha256 } from "js-sha256"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
 const Login = () => {
-  return <div>Hello World! (Login)</div>
+  const [id, setId] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value)
+  }
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  const onClickLogin = () => {
+    console.log(id, sha256(password + sha256(id)), "are pushed")
+  }
+
+  return (
+    <div className="LoginBox">
+      <h2>Login</h2>
+      <input type="text" value={id} onChange={handleId} placeholder="ID" />
+      <input
+        type="password"
+        value={password}
+        onChange={handlePassword}
+        placeholder="Password"
+      />
+      <button onClick={onClickLogin}>Login</button>
+      <Link to="/register">Register</Link>
+    </div>
+  )
 }
 
 export default Login
