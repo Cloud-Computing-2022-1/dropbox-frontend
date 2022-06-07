49fe9bc0456a167d86b7582d735232673df64cdc
Overnap <overnapworks@gmail.com>
2022-06-08 01:38:04 +0900

    Add a redirect to the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "./App.css"
 import Footer from "./components/Footer/Footer"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Login from "./pages/Login/Login"
 import Explorer from "./pages/Explorer/Explorer"
 import Register from "./pages/Register/Register"
@@ -16,6 +16,7 @@ const App = () => {
           <Route path="/register" element={<Register />} />
           <Route path="/explorer/*" element={<Explorer />} />
           <Route path="/search" element={<SearchedExplorer />} />
+          <Route path="/*" element={<Navigate replace to="/login" />} />
         </Routes>
       </BrowserRouter>
       <Footer />
